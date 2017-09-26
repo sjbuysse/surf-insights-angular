dfab87947f3606a7e03709877a4bbf425e7c0c7f
Simon Buysse <simon.buysse@gmail.com>
2017-09-26 16:25:10 +0200

    Add angular google maps component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AgmCoreModule } from '@agm/core';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDGSHuPT1qHuj_3CsFkT5UREkbav2HET-g'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
