5aded877f6ea6239e114f3cb9118ca771e89a4b7
Simon Buysse <simon.buysse@gmail.com>
2017-09-27 14:06:31 +0200

    Add list of names to filter drawer and refactor sass variables to global file

--- src/app/filter-drawer/filter-drawer.component.ts
+++ src/app/filter-drawer/filter-drawer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {SurfspotService} from '../surfspots/surfspot.service';
-import { FirebaseListObservable } from 'angularfire2/database';
+import { FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'si-filter-drawer',
@@ -8,13 +8,22 @@ import { FirebaseListObservable } from 'angularfire2/database';
   styleUrls: ['./filter-drawer.component.scss']
 })
 export class FilterDrawerComponent implements OnInit {
-  surfspots: FirebaseListObservable<any>;
+  surfspots: FirebaseObjectObservable<any>;
+  surfspotList: FirebaseListObservable<any>;
+  showDrawer: boolean;
 
-  constructor(private _service: SurfspotService) { }
+  constructor(private _service: SurfspotService) {
+    this.showDrawer = false;
+  }
+
+  toggleDrawer(): void {
+    this.showDrawer = !this.showDrawer;
+  }
 
   ngOnInit() {
-    this.surfspots = this._service.fetchSurfspots();
-    console.log("init");
+    // this.surfspots = this._service.fetchSurfspots();
+    this.surfspotList = this._service.fetchSurfspotList();
+    this.surfspotList.subscribe(user => console.log(user));
   }
 
 }
--- src/app/surfspots/surfspot.service.ts
+++ src/app/surfspots/surfspot.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Surfspot } from './Surfspot';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import {AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2/database';
 
 @Injectable()
 export class SurfspotService {
   constructor(private _db: AngularFireDatabase) {
   }
-  fetchSurfspots(): FirebaseListObservable<any> {
+  // fetchSurfspots(): FirebaseObjectObservable<any> {
+  //   return this._db.object('/userObjects/places/sVeRmu9z9hTVd6Xf0kOJCwQNuXy1');
+  // }
+  fetchSurfspotList(): FirebaseListObservable<any> {
     return this._db.list('/userObjects/places/sVeRmu9z9hTVd6Xf0kOJCwQNuXy1');
   }
 }
